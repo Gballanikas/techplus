415ed3127bc210cf709480a37f6720a222e2774b
Gballanikas <91003039+Gballanikas@users.noreply.github.com>
2026-09-15 18:49:50 +0400

    Remove unused SciJava repository

--- AstroChart/settings.gradle.kts
+++ AstroChart/settings.gradle.kts
@@ -13,9 +13,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://maven.scijava.org/content/repositories/public/")
-        }
     }
 }
 
