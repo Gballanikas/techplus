724211326c7d6c20eebfc02678b2f5d9e6acf900
Gballanikas <91003039+Gballanikas@users.noreply.github.com>
2026-09-15 19:47:14 +0400

    Fix AstroChart wheel canvas import

--- AstroChart/app/src/main/java/com/gballanikas/astrochart/MainActivity.kt
+++ AstroChart/app/src/main/java/com/gballanikas/astrochart/MainActivity.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
